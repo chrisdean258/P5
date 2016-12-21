b0f8f3e3927d5b0d0477fc77dc74a0855c56beca
chrisdean258 <chrisdean258@gmail.com>
2016-12-21 02:11:55 -0600

    Creating text slider input pairs and consolidating setup file into sketch file

--- RPG_map/sketch.js
+++ RPG_map/sketch.js
@@ -10,3 +10,38 @@ function setup()
   calculateNoise(.01);
   render(1-landSlider.value(), 1-desertSlider.value());
 }
+
+var landSlider;
+var landText;
+var desertSlider;
+var desertText;
+
+function associateDOM()
+{
+  landSlider = select("#landSlider");
+  landText = select("#landText");
+  landSlider.value(.5);
+  landText.value(landSlider.value()+"");
+  landSlider.other = landText;
+  landText.other = landSlider;
+  landText.input(valChanged);
+  landSlider.input(valChanged);
+
+  desertSlider = select("#desertSlider");
+  desertText = select("#desertText");
+  desertSlider.value(.33);
+  desertText.value(desertSlider.value()+"");
+  desertSlider.other = desertText;
+  desertText.other = desertSlider;
+  desertText.input(valChanged);
+  desertSlider.input(valChanged);
+
+
+
+}
+
+function valChanged()
+{
+  this.other.value(this.value());
+  render(1-landSlider.value(), 1-desertSlider.value());
+}
