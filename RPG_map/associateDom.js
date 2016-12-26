50d9469af9d15a70480cf96700912c16a768cddf
chrisdean258 <chrisdean258@gmail.com>
2016-12-25 22:19:14 -0600

    moved dom association to new file

---
+++ RPG_map/associateDom.js
@@ -0,0 +1,74 @@
+var landSlider;
+var landText;
+var desertSlider;
+var desertText;
+var detailSlider;
+var zoomSlider;
+
+function associateDOM()
+{
+  detailSlider = select("#detailSlider");
+  detailText = select("#detailText");
+  detailSlider.value(4);
+  detailText.value(detailSlider.value()+"");
+  detailSlider.other = detailText;
+  detailText.other = detailSlider;
+  detailText.changed(valChangedCalc);
+  detailSlider.changed(valChangedCalc);
+
+  zoomSlider = select("#zoomSlider");
+  zoomText = select("#zoomText");
+  zoomSlider.value(.01);
+  zoomText.value(zoomSlider.value()+"");
+  zoomSlider.other = zoomText;
+  zoomText.other = zoomSlider;
+  zoomText.changed(valChangedCalc);
+  zoomSlider.changed(valChangedCalc);
+  zoomText.hide();
+
+  zSlider = select("#zSlider");
+  zText = select("#zText");
+  zSlider.value(.5);
+  zText.value(zSlider.value()+"");
+  zSlider.other = zText;
+  zText.other = zSlider;
+  zText.changed(valChangedCalc);
+  zSlider.changed(valChangedCalc);
+
+
+  landSlider = select("#landSlider");
+  landText = select("#landText");
+  landSlider.value(.5);
+  landText.value(landSlider.value()+"");
+  landSlider.other = landText;
+  landText.other = landSlider;
+  landText.input(valChangedRender);
+  landSlider.input(valChangedRender);
+
+  desertSlider = select("#desertSlider");
+  desertText = select("#desertText");
+  desertSlider.value(.33);
+  desertText.value(desertSlider.value()+"");
+  desertSlider.other = desertText;
+  desertText.other = desertSlider;
+  desertText.input(valChangedRender);
+  desertSlider.input(valChangedRender);
+}
+
+function valChangedRender()
+{
+  if(this.other != undefined)
+  {
+    this.other.value(this.value());
+  }
+  render();
+}
+function valChangedCalc()
+{
+  if(this.other != undefined)
+  {
+    this.other.value(this.value());
+  }
+  calculateNoise()
+  render();
+}
