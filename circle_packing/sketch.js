9308b8d9234316914df6bcf26c98ea586e406282
chrisdean258 <chrisdean258@gmail.com>
2017-01-16 19:23:11 -0500

    Circle Packing:added file drop handling

--- circle_packing/sketch.js
+++ circle_packing/sketch.js
@@ -1,11 +1,23 @@
+var dropzone;
 
 function setup() 
 {
+  dropzone = select('#dropzone');
+  dropzone.dragOver(function(){highlight(dropzone,'#CCC')});
+  dropzone.dragLeave(function(){highlight(dropzone,'#FFF')});
+  dropzone.drop(loadImage,function(){highlight(dropzone,'#FFF')});
 }
 
-
-function draw() 
+function highlight(domElem,color)
 {
+  domElem.style("background-color",color);
 }
+
+function loadImage(image)
+{
+  console.log(image);
+}
+
+
 
 
